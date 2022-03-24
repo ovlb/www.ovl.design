8b2d1f766d41464741b4b1e3c6d318860d1bd3f5
Oscar <o@ovl.design>
2022-03-24 10:03:57 +0100

    📚 add archive components

---
+++ _src/_includes/components/archive/ArchiveHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="archive-header u-floral-heart-gradient">
+    <h1 class="main-headline">{{ title }}</h1>
+    <slot name="sub" />
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ _src/_includes/components/archive/ArticleCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <article class="article-card" aria-labelledby="title-{{ id }}">
+    <p class="type-is-aside"><slot name="eyebrow" /></p>
+    <h2 id="title-{{ id }}" class="article-card__headline">
+      <a href="{{ data.permalink }}" class="article-card__link">
+        {{ data.title }}
+      </a>
+    </h2>
+    <p>{{ data.intro }}</p>
+  </article>
+</template>
+
+<script>
+import { v4 as uuidv4 } from 'uuid'
+
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      id: uuidv4(),
+    }
+  },
+}
+</script>
+
+<style></style>
