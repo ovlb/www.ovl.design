8ffe2a4d2024b49ae93d90cf45eae1d2a2058f95
Oscar <o@ovl.design>
2022-03-24 12:16:07 +0100

    📆 flexible date formatter

--- _filters/display-date.js
+++ _filters/display-date.js
@@ -1,6 +1,12 @@
-module.exports = function (date) {
-  return new Date(date).toLocaleDateString('en-UK', {
-    month: 'long',
-    year: 'numeric',
-  })
+module.exports = function (date, format = 'long') {
+  date = typeof date === 'string' ? new Date(date) : date
+
+  if (format === 'long') {
+    return date.toLocaleDateString('en-UK', {
+      month: 'long',
+      year: 'numeric',
+    })
+  }
+
+  return date.toLocaleDateString('de')
 }
