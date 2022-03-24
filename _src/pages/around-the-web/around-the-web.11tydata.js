e1f8ad595915deab4a5826e08315215d0930d677
Oscar <o@ovl.design>
2022-03-24 12:11:53 +0100

    🖇  abstract templkate data

---
+++ _src/pages/around-the-web/around-the-web.11tydata.js
@@ -0,0 +1,13 @@
+module.exports = {
+  templateClass: 'tmpl-article-list',
+  categoryBase: '/around-the-web/collections',
+  pageCSS: 'text',
+  meta: {
+    description:
+      'Around the Web is a (weekly) digest of interesting links from, you might have guessed it, around the web.',
+    image: {
+      src: 'https://www.ovl.design/img/around-the-web/archive.jpg',
+      alt: 'A library in Prague. The ceiling is decorated with magnificent frescoes. The shelfes are old, you can smell the history contained in these books, it oozes from the picture. Written on top are the words «Around the web».',
+    },
+  },
+}
