20a3b9a7aae7590ea181206a14e7546feb011d9d
Oscar <o@ovl.design>
2022-03-25 08:14:35 +0100

    🐰 add og:url, automatically merge meta objects

--- _shortcodes/meta-tags.js
+++ _shortcodes/meta-tags.js
@@ -1,24 +1,27 @@
-const path = require('path')
-const { meta: SITE_DEFAULTS } = require(path.join(
-  process.cwd(),
-  '_src/_data/site',
-))
-
-const tags = [
-  { attrName: 'name', attrValue: 'description', key: 'description' },
-  { attrName: 'name', attrValue: 'twitter:description', key: 'description' },
-  { attrName: 'property', attrValue: 'og:description', key: 'description' },
-  { attrName: 'name', attrValue: 'twitter:card', value: 'summary_large_image' },
-  { attrName: 'name', attrValue: 'twitter:site', value: '@_ovlb' },
-  { attrName: 'name', attrValue: 'twitter:creator', value: '@_ovlb' },
-  { attrName: 'property', attrValue: 'og:type', key: 'ogType' },
-  { attrName: 'property', attrValue: 'og:image:type', key: 'ogImageType' },
-  { attrName: 'property', attrValue: 'og:image:width', value: '1478' },
-  { attrName: 'property', attrValue: 'og:image:height', value: '831' },
-  { attrName: 'property', attrValue: 'og:site_name', key: 'site_name' },
-]
-
 module.exports = function (meta = {}, post) {
+  this.tags = [
+    { attrName: 'name', attrValue: 'description', key: 'description' },
+    { attrName: 'name', attrValue: 'twitter:description', key: 'description' },
+    { attrName: 'property', attrValue: 'og:description', key: 'description' },
+    {
+      attrName: 'property',
+      attrValue: 'og:url',
+      value: `${this.ctx.site.baseURL}${this.page.url}`,
+    },
+    {
+      attrName: 'name',
+      attrValue: 'twitter:card',
+      value: 'summary_large_image',
+    },
+    { attrName: 'name', attrValue: 'twitter:site', value: '@_ovlb' },
+    { attrName: 'name', attrValue: 'twitter:creator', value: '@_ovlb' },
+    { attrName: 'property', attrValue: 'og:type', key: 'ogType' },
+    { attrName: 'property', attrValue: 'og:image:type', key: 'ogImageType' },
+    { attrName: 'property', attrValue: 'og:image:width', value: '1478' },
+    { attrName: 'property', attrValue: 'og:image:height', value: '831' },
+    { attrName: 'property', attrValue: 'og:site_name', key: 'site_name' },
+  ]
+
   if (post) {
     if (post.openGraphImage) {
       meta.image = {
@@ -31,19 +34,17 @@ module.exports = function (meta = {}, post) {
     meta.ogType = 'article'
   }
 
-  const values = { ...SITE_DEFAULTS, ...meta }
+  // const values = { ...SITE_DEFAULTS, ...meta }
 
   const img = `
-    <meta property="og:image" content="${values.image.src}?w=1478&h=831">
-    <meta name="twitter:image" content="${values.image.src}?w=1478&h=831&fm=jpg&q=100">
-    <meta name="twitter:image:alt" content="${values.image.alt}">
+    <meta property="og:image" content="${meta.image.src}?w=1478&h=831">
+    <meta name="twitter:image" content="${meta.image.src}?w=1478&h=831&fm=jpg&q=100">
+    <meta name="twitter:image:alt" content="${meta.image.alt}">
   `
 
-  const rendered = tags
+  const rendered = this.tags
     .map(({ attrName, attrValue, key, value }) => {
-      return `<meta ${attrName}="${attrValue}" content="${
-        value ?? values[key]
-      }">`
+      return `<meta ${attrName}="${attrValue}" content="${value ?? meta[key]}">`
     })
     .join('')
 
--- _src/_data/site.js
+++ _src/_data/site.js
@@ -22,17 +22,4 @@ module.exports = {
   strings: {
     skip_to_content: 'Skip to main content',
   },
-  meta: {
-    image: {
-      src: 'https://images.ctfassets.net/0qq78o7muy2j/41AVLQd3q0oEaQKwcW0Ck2/1adc1e0fc0c7525b4f25b45570847396/ovl-og-image-generic.png',
-      alt: 'An owl sitting on a branch. The letters o v l are added in the top right corner.',
-      width: 2240,
-      height: 1260,
-    },
-    ogType: 'blog',
-    ogImageType: 'image/png',
-    site_name: 'www.ovl.design',
-    description:
-      'Hi, I am Oscar. I write code and design for web and print. And this is my website.',
-  },
 }
