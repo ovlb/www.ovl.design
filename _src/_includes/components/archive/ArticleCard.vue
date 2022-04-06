5b707b9b890fcea99b08c93bfe4846a2568352c9
Oscar <o@ovl.design>
2022-04-06 22:31:01 +0200

    📚 render archive with vue

--- _src/_includes/components/archive/ArticleCard.vue
+++ _src/_includes/components/archive/ArticleCard.vue
@@ -1,12 +1,14 @@
 <template>
-  <article class="article-card" aria-labelledby="title-{{ id }}">
+  <article class="article-card" :aria-labelledby="`title-${id}`">
     <p class="type-is-aside"><slot name="eyebrow" /></p>
-    <h2 id="title-{{ id }}" class="article-card__headline">
+    <h2 :id="`title-${id}`" class="article-card__headline">
       <a :href="data.permalink" class="article-card__link">
         {{ data.title }}
       </a>
     </h2>
+    <p class="article-card__date type-is-aside"><slot name="date" /></p>
     <p>{{ data.intro }}</p>
+    <p class="type-is-aside"><slot name="footer" /></p>
   </article>
 </template>
 
