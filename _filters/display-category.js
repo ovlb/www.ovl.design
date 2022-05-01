de351d245b235bd5bdbb214d9cd0f6d02ad2bda7
Oscar <o@ovl.design>
2022-05-01 12:03:39 +0200

    🖇 cat:crypto display name

--- _filters/display-category.js
+++ _filters/display-category.js
@@ -3,6 +3,7 @@ const { startCase, camelCase } = require('lodash')
 module.exports = function (rawName) {
   const displayNames = new Map([
     ['cat:ai', 'Artificial (Un)intelligence'],
+    ['cat:crypto', 'The road to hell is paved with crypto intentions'],
     ['cat:nft', 'NFT'],
     ['cat:climate', 'Climate Crisis'],
     ['cat:web', 'World Wide Web'],
