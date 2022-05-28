0d05969cf3bb6ea8f89e6dd0d0062ee34e62d595
Oscar <o@ovl.design>
2022-05-28 22:45:14 +0200

    📊 around the numbers

---
+++ _src/_includes/components/stats/FeaturedStat.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="featured-stat">
+    <b class="main-headline">{{ stat }}</b>
+    <p>{{ text }}</p>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    stat: {
+      type: [String, Number],
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+.featured-stat {
+  text-align: center;
+}
+</style>
