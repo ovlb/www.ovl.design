19c1a741ae0449b3f99963a3ab5eec938561c31f
Oscar <o@ovl.design>
2022-09-30 14:22:32 +0200

    🔗 update claim

--- _src/pages/around-the-web/around-the-web.11tydata.js
+++ _src/pages/around-the-web/around-the-web.11tydata.js
@@ -4,7 +4,7 @@ module.exports = {
   pageCSS: 'text',
   meta: {
     description:
-      'Around the Web is a (weekly) digest of interesting links from – you might have guessed it – around the web.',
+      'A digest of interesting links from – you might have guessed it – around the web.',
     image: {
       src: 'https://www.ovl.design/img/around-the-web/archive.jpg',
       alt: 'A library in Prague. The ceiling is decorated with magnificent frescoes. The shelfes are old, you can smell the history contained in these books, it oozes from the picture. Written on top are the words «Around the web».',
