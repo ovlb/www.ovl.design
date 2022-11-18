9f52b665180b5ef49cfb2fb41c1cb7069ba02161
Oscar <o@ovl.design>
2022-11-18 14:08:13 +0100

    📚 rewrite feed

---
+++ _src/pages/text/feed.11ty.js
@@ -0,0 +1,76 @@
+const { Feed } = require('feed')
+const { baseURL } = require(`${process.cwd()}/_src/_data/site`)
+const {
+  absoluteUrl,
+  convertHtmlToAbsoluteUrls,
+  getNewestCollectionItemDate,
+} = require('@11ty/eleventy-plugin-rss')
+
+module.exports = class TextFeed {
+  data() {
+    return {
+      layout: null,
+      permalink: '/text/feed.xml',
+      eleventyExcludeFromCollections: true,
+      collection: 'blog',
+      metadata: this.metadata,
+    }
+  }
+
+  get feedURL() {
+    return `${baseURL}${this.permalink}`
+  }
+
+  get metadata() {
+    return {
+      title: 'ovl.design » text',
+      subtitle: 'mostly coherent scribbles on digital paper',
+      feedID: '/text',
+    }
+  }
+
+  makeFeed(baseData, collection) {
+    return new Feed({
+      ...baseData,
+      ...this.metadata,
+      id: this.feedURL,
+      link: absoluteUrl(`${this.metadata.feedID}/`, baseURL),
+      updated: getNewestCollectionItemDate(collection),
+      feedLinks: {
+        json: this.feedURL,
+        atom: this.feedURL,
+      },
+    })
+  }
+
+  async render({ feed: feedData, collections, site }) {
+    const { blog } = collections
+
+    const feed = this.makeFeed(feedData, blog)
+
+    for (const post of blog.reverse()) {
+      let link = absoluteUrl(post.data.permalink, site.baseURL)
+
+      if (post.data.external) {
+        link = post.data.external.source
+      }
+
+      feed.addItem({
+        title: post.data.title,
+        link,
+        id: link,
+        date: new Date(post.data.date),
+        description: post.data.displayIntro,
+        image: post.data.meta.image.src,
+        ...(post.templateContent && {
+          content: await convertHtmlToAbsoluteUrls(
+            await this.feedImages(this.fixCite(post.templateContent)),
+            this.feedURL,
+          ),
+        }),
+      })
+    }
+
+    return feed.atom1()
+  }
+}
