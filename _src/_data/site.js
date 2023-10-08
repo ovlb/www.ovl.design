bafedfc3af2a80104e952874ff4d4e5ae95df7a3
Oscar <o@ovl.design>
2023-10-08 22:37:51 +0200

    🤷 rm twitter

--- _src/_data/site.js
+++ _src/_data/site.js
@@ -31,7 +31,6 @@ module.exports = {
       rel: 'me',
       logo: '/assets/img/mastodon_gradient.svg',
     },
-    { title: 'Twitter', url: 'https://twitter.com/_ovlb', rel: 'me' },
   ],
   metaImage:
     'https://images.ctfassets.net/0qq78o7muy2j/41AVLQd3q0oEaQKwcW0Ck2/1adc1e0fc0c7525b4f25b45570847396/ovl-og-image-generic.png',
