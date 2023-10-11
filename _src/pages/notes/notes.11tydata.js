66c8c3fb8e2924e34b57fe506999a4b1331ada63
Oscar <o@ovl.design>
2023-10-11 14:58:31 +0200

    📓 add og meta data

--- _src/pages/notes/notes.11tydata.js
+++ _src/pages/notes/notes.11tydata.js
@@ -1,3 +1,10 @@
 module.exports = {
   categoryBase: '/notes/collections',
+  meta: {
+    description: 'Pages in a virtual notebook.',
+    image: {
+      src: `${process.env.BASE_URL}/img/open-graph/og-image-notes-v1.jpg`,
+      alt: 'A book and a cup of of tea on a desk made of stone. A pencil lies to the right of the book. The books has been used often, it’s pages are slightly worn and the case isn’t closing tightly anymore.',
+    },
+  },
 }
