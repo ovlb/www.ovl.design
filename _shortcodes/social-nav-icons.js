1746eb24579e14b052d7c5e0f55500b6db556cb0
Oscar <o@ovl.design>
2023-10-14 16:04:56 +0200

    🕵️ fix linting

--- _shortcodes/social-nav-icons.js
+++ _shortcodes/social-nav-icons.js
@@ -4,8 +4,8 @@ module.exports = function (items) {
         .map(({ url, title, rel }) => {
           return `<li>
             <a href="${url}" class="vertical-nav__link -is-${title.toLowerCase()}" ${
-            rel && `rel=${rel}`
-          }>${title}</a>`
+              rel && `rel=${rel}`
+            }>${title}</a>`
         })
         .join('')}
     </ul>`
