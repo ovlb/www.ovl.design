3ca02ec874b2330d6ae9c81575d6ecab124a94b8
Oscar <o@ovl.design>
2023-10-16 12:53:14 +0200

    📂 add capitalisation for js cat

--- _filters/capitaliser.js
+++ _filters/capitaliser.js
@@ -3,11 +3,12 @@ const { startCase, camelCase } = require('lodash')
 module.exports = function (rawName) {
   const displayNames = new Map([
     ['cat:ai', 'Artificial (Un)intelligence'],
-    ['cat:crypto', 'The road to hell is paved with crypto intentions'],
-    ['cat:nft', 'NFT'],
     ['cat:climate', 'Climate Crisis'],
-    ['cat:web', 'World Wide Web'],
+    ['cat:crypto', 'The road to hell is paved with crypto intentions'],
     ['cat:css', 'CSS'],
+    ['cat:javascript', 'JavaScript'],
+    ['cat:nft', 'NFT'],
+    ['cat:web', 'World Wide Web'],
     ['around-the-web', 'Around the Web'],
   ])
 
